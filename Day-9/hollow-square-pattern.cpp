15173534b7746333a4c8a3ec95acc69dc74ace1a
AYUSH YADAV <ayushyad176@gmail.com>
2026-06-05 18:09:44 +0530

    uploaded Day 9

---
+++ Day-9/hollow-square-pattern.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int num;
+
+    cout<<"enter size of square : ";
+    cin>>num;
+
+    for(int i=1; i<=num; i++) {
+
+        for(int j=1; j<=num; j++) {
+
+            if(i == 1 || i == num || j == 1 || j == num)
+            cout<< "* ";
+
+            else
+            cout<<"  ";
+        }
+
+        cout<<endl ;
+    }
+
+
+    return 0;
+}
