22ea0f7de6a965dfb7651b6f14445cbd212af94c
AYUSH YADAV <ayushyad176@gmail.com>
2026-06-06 17:08:33 +0530

    uploaded Day 10

---
+++ Day-10/reverse-pyramid-pattern.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int rows;
+
+    cout<<"enter number of rows : ";
+    cin>>rows;
+
+    for(int i=rows; i>=1; i--) {
+
+        for(int j=1; j<= rows - i; j++) {
+
+            cout<<" ";
+        }
+
+        for(int j=1; j<= (2 * i - 1); j++) {
+
+            cout<<"*";
+
+        }
+
+        cout<<endl ;
+    }
+
+    return 0;
+}
