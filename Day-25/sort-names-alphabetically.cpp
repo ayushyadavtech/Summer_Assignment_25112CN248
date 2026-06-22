deb982aeb2fd8c0367501e55bd491d887c71d6e3
AYUSH YADAV <ayushyad176@gmail.com>
2026-06-22 10:40:06 +0530

    uploaded Day 25

---
+++ Day-25/sort-names-alphabetically.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cout << "enter number of names : ";
+    cin >> n;
+
+    string names[n];
+
+    cin.ignore();
+
+    cout << "Enter names: \n";
+
+    for (int i = 0; i < n; i++) {
+        getline(cin, names[i]);
+    }
+
+    // Sorting names alphabetically
+    for (int i = 0; i < n - 1; i++) {
+
+        for (int j = i + 1; j < n; j++) {
+
+            if (names[i] > names[j]) {
+                string temp = names[i];
+                names[i] = names[j];
+                names[j] = temp;
+            }
+        }
+    }
+
+    cout << "\nNames in alphabetical order:\n";
+    
+    for (int i = 0; i < n; i++) {
+        cout << names[i] << endl;
+    }
+
+    return 0;
+}
